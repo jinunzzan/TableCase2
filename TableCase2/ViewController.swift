4b17204700dcc0b1d23ba25598fbbf16312ea20a
jinunzzan <lovebox6607@gmail.com>
2022-10-06 19:02:34 +0900

    Initial Commit

---
+++ TableCase2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableCase2
+//
+//  Created by Eunchan Kim on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
